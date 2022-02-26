6428b79050c76bc834043563766209a23f87bf2e
aktivgo <aktivgo01062001@gmail.com>
2022-02-26 09:59:33 +0300

    Add retry policy to mysql connect

--- internal/storage/mysql/mysql.go
+++ internal/storage/mysql/mysql.go
@@ -3,13 +3,24 @@ package mysql
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"log"
+	"strconv"
+	"time"
 )
 
 func CreateConnection(mysqlConnStr string) (*sqlx.DB, error) {
-	mysqlConn, err := sqlx.Connect("mysql", mysqlConnStr)
-	if err != nil {
-		return nil, err
+	var mysqlConn *sqlx.DB
+	var err error
+
+	for i := 0; i < 5; i++ {
+		log.Println("Try to connect to mysql... Attempt " + strconv.Itoa(i+1))
+		mysqlConn, err = sqlx.Connect("mysql", mysqlConnStr)
+		if err == nil {
+			log.Println("Connection with mysql created")
+			return mysqlConn, nil
+		}
+		time.Sleep(2 * time.Second)
 	}
 
-	return mysqlConn, nil
+	return nil, err
 }
